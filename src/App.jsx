b2c578fcc95c5f49a3e11bd9f26c4dac19a5cbbe
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-09 13:57:09 -0500

    Project creaeted and controller added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {Route} from 'react-router-dom'
+import { UsersButton } from './components/UsersButton'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Route path= "/users"></Route>
+    </>
+  )
+}
+
+export default App
