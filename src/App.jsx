292de0cb0b0d30bbbac1a24cba7a0e502c008635
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-09 19:29:38 -0500

    added router, routes and route libreries and segmented each functional path

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import {Route} from 'react-router-dom'
-import { UsersButton } from './components/UsersButton'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './components/Home'
+import { UsersPage } from './components/UsersPage'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    <Route path= "/users"></Route>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<UsersPage />} />
+      </Routes>
+    </Router>
   )
 }
 
