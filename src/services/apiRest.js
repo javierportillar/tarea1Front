0fb2a8c6648b5eddf495d19173432265e13efc8e
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-09 19:42:43 -0500

    added catch error

--- src/services/apiRest.js
+++ src/services/apiRest.js
@@ -1,5 +1,7 @@
 export const fetchApi = () => {
-  return fetch("http://localhost:4400/users").then((response) => {
-    return response.json();
-  });
+  return fetch("http://localhost:4400/users")
+    .catch(err => console.log(err))
+    .then((response) => {
+      return response.json();
+    });
 };
