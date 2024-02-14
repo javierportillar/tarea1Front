058dc1c322c653ee42c1a75cec5fd532409e3a3d
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-13 19:14:55 -0500

    updated UsersTable component and call UsersRow component for each user

---
+++ src/components/users/usersTable/UsersTable.component.jsx
@@ -0,0 +1,22 @@
+import './UsersTable.component.css'
+import { UsersRow } from "../usersRow/UsersRow.component";
+
+export const UsersTable = () => {
+
+  return (
+    <div className="user-table">
+      <table>
+        <thead>
+          <tr>
+            <td>name</td>
+            <td>id</td>
+            <td>age</td>
+          </tr>
+        </thead>
+        <tbody>
+          <UsersRow/>
+        </tbody>
+      </table>
+    </div>
+  );
+};
