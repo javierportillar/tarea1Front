aef88612da356c85d4f9523602e2ab6fcafc74b1
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-14 16:23:04 -0500

    Updated Loading and error return

--- src/components/users/usersRow/UsersRow.component.jsx
+++ src/components/users/usersRow/UsersRow.component.jsx
@@ -4,32 +4,31 @@ import './UsersRow.component.css'
 
 export const UsersRow = () => {
   const { loading, error, data } = useUsers();
-  const [users, setUsers] = useState([]);
+
+    if (loading) {
+      return (
+        <tr>
+          <td colSpan="3">Loading...</td>
+        </tr>
+      );
+    }
+
+    if (error) {
+      return (
+        <tr>
+          <td colSpan="3">Error: {error}</td>
+        </tr>
+      );
+    }
+
+    return (
+      data.map((user, index) => (
+        <tr key={index} className="user-row">
+          <td>{user.name}</td>
+          <td>{user.id}</td>
+          <td>{user.age}</td>
+        </tr>
+      ))
+    )
   
-  if (loading) {
-    return (
-      <tr>
-        <td colSpan="3">Loading...</td>
-      </tr>
-    );
-  }
-
-  if (error) {
-    return (
-      <tr>
-        <td colSpan="3">Error: {error}</td>
-      </tr>
-    );
-  }
-
-  return (
-    data.map((user, index) => (
-      <tr key={index} className="user-row">
-        <td>{user.name}</td>
-        <td>{user.id}</td>
-        <td>{user.age}</td>
-      </tr>
-    ))
-  )
-
 }
