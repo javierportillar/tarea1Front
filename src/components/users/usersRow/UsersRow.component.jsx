d61e6708855a4ba3418473d7bd32c95f9ac908aa
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-13 19:17:51 -0500

    Created Loading and Error States for respective return, mapped data info into td for name, id, age into a tr element

---
+++ src/components/users/usersRow/UsersRow.component.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useUsers } from "../../../hooks/useUser.hook";
+import './UsersRow.component.css'
+
+export const UsersRow = () => {
+  const { loading, error, data } = useUsers();
+  const [users, setUsers] = useState([]);
+  
+  if (loading) {
+    return (
+      <tr>
+        <td colSpan="3">Loading...</td>
+      </tr>
+    );
+  }
+
+  if (error) {
+    return (
+      <tr>
+        <td colSpan="3">Error: {error}</td>
+      </tr>
+    );
+  }
+
+  return (
+    data.map((user, index) => (
+      <tr key={index} className="user-row">
+        <td>{user.name}</td>
+        <td>{user.id}</td>
+        <td>{user.age}</td>
+      </tr>
+    ))
+  )
+
+}
