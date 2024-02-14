b9bfea9a6d8a1346b079f661c5a305b3f255cf06
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-14 17:27:18 -0500

    added useCreateUserService for making post request, it receives userData as an object and by bodu: JSON.sringify(userData) send the request under body as json format, in case of receiving and error,it retirms error with error message

--- src/services/users.service.jsx
+++ src/services/users.service.jsx
@@ -3,3 +3,24 @@ const url = 'http://localhost:4400/users'
 export const useUsersService = () => {
   return fetch(url).then(datos => datos.json())
 }
+
+export const useCreateUserService = (userData) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .catch(error => {
+    console.error("Error in fetch: ", error);
+    throw error;
+  });
+};
+
