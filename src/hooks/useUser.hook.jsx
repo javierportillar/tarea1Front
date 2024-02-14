b2854dc6999224e52e029abe3a67168f421f8911
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-14 17:24:35 -0500

    hook updated with useReateUser fcn, this is in charge of receive the post request and send it to the service fcn, this needed to create an const isMounted = useRef, because we need the component to be rendered once it receives an userData objet, and at the first time, the component is mounted but its not needed to render the fcn

--- src/hooks/useUser.hook.jsx
+++ src/hooks/useUser.hook.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useUsersService } from "../services/users.service";
+import { useCreateUserService } from "../services/users.service";
 
 export const useUsers = () => {
   const [loading, setLoading] = useState(false);
@@ -21,4 +22,35 @@ export const useUsers = () => {
   }, []);
 
   return { loading, error, data };
+}
+
+export const useCreateUser = (userData) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if(isMounted.current) {
+      isMounted.current = false;
+      return;
+    }
+
+    if (userData && userData.name && userData.age) {
+      setLoading(true);
+      useCreateUserService(userData)
+        .then(() => setLoading(false))
+        .catch(err => {
+          setError(err.message);
+          setLoading(false);
+        });
+    }
+  }, [userData]);
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  return { loading, error };
 }
