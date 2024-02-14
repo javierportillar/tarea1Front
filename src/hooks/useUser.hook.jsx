9383b74dfad1b309a60980e60487402da8e57446
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-13 19:08:12 -0500

    updated hook for loading, error and data returning

---
+++ src/hooks/useUser.hook.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { useUsersService } from "../services/users.service";
+
+export const useUsers = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    useUsersService()
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  return { loading, error, data };
+}
