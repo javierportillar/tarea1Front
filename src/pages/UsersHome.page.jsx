8ec9a186aa9d05bc6d5f7642aef4244aa3378762
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-26 21:17:47 -0500

    added fcn for open and close diff divs sending states changes, added userEditComponent as a component rendered by an sate

--- src/pages/UsersHome.page.jsx
+++ src/pages/UsersHome.page.jsx
@@ -3,11 +3,31 @@ import { UsersTable } from "../components/users/usersTable/UsersTable.component"
 import { NewUserButton } from "../components/users/newUserButton/newUserButton.component";
 import { NewUser } from "../components/users/newUser/NewUser.component";
 import { UserProvider } from "../context/userContext.context";
+import { UserEditComponent } from "../components/usersOptions/userEdit/UserEdit.component";
 import './usersHome.page.css';
 
 export const UsersHomePage = () => {
 
   const [addUserState, setAddUserState] = useState(false);
+  const [editUserState, setEditUserState] = useState(false);
+  const [deletUserState, setDeletUserState] = useState(false);
+
+  const openEditUserState = () => {
+    setEditUserState(true);
+  }
+
+  const closeEditUserState = () => {
+    setEditUserState(false);
+  }
+
+  const openDeletUserState = () => {
+    
+  }
+
+  const closeDeletUserState = () => {
+
+  }
+
 
   const openCreateNewUserDiv = () => {
     setAddUserState(true);
@@ -21,9 +41,10 @@ export const UsersHomePage = () => {
     <div className="user-page">
       <h1 className="user-tittle">Los usuarios son los siguientes:</h1>
       <UserProvider>
-        <UsersTable />
+        <UsersTable openEditUserState={openEditUserState} />
         <NewUserButton openCreateNewUserDiv={openCreateNewUserDiv} />
         {addUserState && (<NewUser closeCreateNewUser={closeCreateNewUser} />)}
+        {editUserState && (<UserEditComponent closeEditUserState = {closeEditUserState}/>)}
       </UserProvider>
     </div>
   )
