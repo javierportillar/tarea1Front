5f4123db4ac8e8f6e475645d2be8b4349d4a563b
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-28 12:11:45 -0500

    Created Delete component, receives selected user form the hook and deleteUser for send delete request

---
+++ src/components/usersOptions/userDelete/deleteUser.component.jsx
@@ -0,0 +1,26 @@
+import './deleteUser.component.css'
+import { useUserContext } from '../../../context/userContext.context'
+import { DeleteUserButton } from '../deleteUserButton/deleteUserButton';
+
+export const DeleteUser = ({ closeDeletUserState }) => {
+
+  const { selectedUser, deleteUser } = useUserContext();
+
+
+  return (
+    <div className='delete-user-window'>
+      <div className='delete-user'>
+        <button id='close-user-inputs' onClick={closeDeletUserState}>X</button>
+        <h1>la información de _ se eliminará permanentemente</h1>
+        <div className='info-user-delete'>
+          <h2>información del usuario:</h2>
+          <p>id:{selectedUser.id}</p>
+          <p>name:{selectedUser.name}</p>
+          <p>age:{selectedUser.age}</p>
+        </div>
+        <h2>¿Está segur@ que desea confirmar los cambios?</h2>
+        <DeleteUserButton selectedUser={selectedUser} deleteUser={deleteUser} closeDeletUserState={closeDeletUserState}/>
+      </div>
+    </div>
+  )
+}
