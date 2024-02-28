ab0815bfd33311aa5d0ab613196794a159fbf11d
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-28 11:56:16 -0500

    modified getuserFcn for receiving string as param and change state of editDiv for render edit or delete component

--- src/components/users/usersRow/UsersRow.component.jsx
+++ src/components/users/usersRow/UsersRow.component.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import './UsersRow.component.css'
 import { useUserContext } from "../../../context/userContext.context";
 
-export const UsersRow = ({ openEditUserState }) => {
+export const UsersRow = ({ openEditUserState, openDeletUserState }) => {
 
   const { loading, error, users, userChoosed } = useUserContext();
 
@@ -22,9 +22,13 @@ export const UsersRow = ({ openEditUserState }) => {
     );
   }
 
-  const getUserFcn = (user) => {
-      userChoosed(user);
+  const getUserFcn = (user, param) => {
+    userChoosed(user);
+    if (param === 'edit') {
       openEditUserState();
+    } else{
+      openDeletUserState();
+    }
   }
 
   return (
@@ -35,9 +39,11 @@ export const UsersRow = ({ openEditUserState }) => {
         <td>{user.age}</td>
         <td>
           <button className="user-button" onClick={() => {
-            getUserFcn(user);
+            getUserFcn(user, 'edit');
           }}>Edit</button>
-          <button className="user-button">Delete</button>
+          <button className="user-button" onClick={() => {
+            getUserFcn(user, 'delete');
+          }}>Delete</button>
         </td>
       </tr>
     ))
