7f2366398a28ff65bfc128910ec69805c939ce4e
“javierportillar” <javierandres.008@hotmail.com>
2024-03-07 09:00:27 -0500

    Refactor user page logic managment for components show

--- src/pages/UsersHome.page.jsx
+++ src/pages/UsersHome.page.jsx
@@ -1,52 +1,23 @@
-import { useState } from "react";
 import { UsersTable } from "../components/users/usersTable/UsersTable.component";
 import { NewUserButton } from "../components/users/newUserButton/newUserButton.component";
 import { NewUser } from "../components/users/newUser/NewUser.component";
-import { UserProvider } from "../context/userContext.context";
+import { useUserContext } from "../context/userContext.context";
 import { UserEditComponent } from "../components/usersOptions/userEdit/userEdit.component";
 import { DeleteUser } from "../components/usersOptions/userDelete/deleteUser.component";
 import './usersHome.page.css';
 
 export const UsersHomePage = () => {
 
-  const [addUserState, setAddUserState] = useState(false);
-  const [editUserState, setEditUserState] = useState(false);
-  const [deletUserState, setDeletUserState] = useState(false);
-
-  const openEditUserState = () => {
-    setEditUserState(true);
-  }
-
-  const closeEditUserState = () => {
-    setEditUserState(false);
-  }
-
-  const openDeletUserState = () => {
-    setDeletUserState(true)
-  }
-
-  const closeDeletUserState = () => {
-    setDeletUserState(false)
-  }
-
-  const openCreateNewUserDiv = () => {
-    setAddUserState(true);
-  }
-
-  const closeCreateNewUser = () => {
-    setAddUserState(false);
-  }
+  const { addUserState, editUserState, deletUserState } = useUserContext();
 
   return (
     <div className="user-page">
       <h1 className="user-tittle">Los usuarios son los siguientes:</h1>
-      <UserProvider>
-        <UsersTable openEditUserState={openEditUserState} openDeletUserState={openDeletUserState}/>
-        <NewUserButton openCreateNewUserDiv={openCreateNewUserDiv} />
-        {addUserState && (<NewUser closeCreateNewUser={closeCreateNewUser} />)}
-        {editUserState && (<UserEditComponent closeEditUserState = {closeEditUserState}/>)}
-        {deletUserState && (<DeleteUser closeDeletUserState = {closeDeletUserState}/>)}
-      </UserProvider>
+      <UsersTable />
+      <NewUserButton />
+      {addUserState && (<NewUser />)}
+      {editUserState && (<UserEditComponent />)}
+      {deletUserState && (<DeleteUser />)}
     </div>
   )
 }
--- src/services/users.service.jsx
+++ src/services/users.service.jsx
@@ -1,28 +1,29 @@
 const url = 'http://localhost:4400/users/'
 
 export const fetchUsersService = () => {
-  return fetch(url).then(datos => datos.json())
+  return fetch(url)
+  .then(datos => datos.json())
 }
 
 export const updateUserService = (id, userData) => {
-  const urlFetch = url+id;
-  return fetch(urlFetch,{
+  const urlFetch = url + id;
+  return fetch(urlFetch, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(userData),
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .catch(error => {
-    console.error("Error in fetch: ", error);
-    throw error;
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error("Error in fetch: ", error);
+      throw error;
+    });
 }
 
 export const createUserService = (userData) => {
@@ -33,19 +34,27 @@ export const createUserService = (userData) => {
     },
     body: JSON.stringify(userData),
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .catch(error => {
-    console.error("Error in fetch: ", error);
-    throw error;
-  });
-};
-
-export const deleteUserService = (id) => {
-  const urlFetch = url + id;
-  return fetch(urlFetch, { method: 'DELETE' })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error("Error in fetch: ", error);
+      throw error;
+    });
+  };
+  
+  export const deleteUserService = (id) => {
+    const urlFetch = url + id;
+    return fetch(urlFetch, { method: 'DELETE' })
+    .then(response => {
+      if(!response.ok){
+        throw new Error('Network response was not ok');
+      }
+    }).catch(error=>{
+      console.error("Error fetching: ", error)
+      throw error;
+    })
 }
