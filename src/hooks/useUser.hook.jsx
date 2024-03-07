07e847665094a4fb00d11f4499640ef4dd5cc0e3
“javierportillar” <javierandres.008@hotmail.com>
2024-03-07 08:58:37 -0500

    Add user state management and error handling to useUsers hook

--- src/hooks/useUser.hook.jsx
+++ src/hooks/useUser.hook.jsx
@@ -2,6 +2,38 @@ import { useState, useEffect } from 'react';
 import { fetchUsersService, createUserService, deleteUserService, updateUserService } from '../services/users.service';
 
 export const useUsers = () => {
+
+  // Loading Home Page //
+
+  const [addUserState, setAddUserState] = useState(false);
+  const [editUserState, setEditUserState] = useState(false);
+  const [deletUserState, setDeletUserState] = useState(false);
+
+  const openEditUserState = () => {
+    setEditUserState(true);
+  }
+
+  const closeEditUserState = () => {
+    setEditUserState(false);
+  }
+
+  const openDeletUserState = () => {
+    setDeletUserState(true)
+  }
+
+  const closeDeletUserState = () => {
+    setDeletUserState(false)
+  }
+
+  const openCreateNewUserDiv = () => {
+    setAddUserState(true);
+  }
+
+  const closeCreateNewUser = () => {
+    setAddUserState(false);
+  }
+
+
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -9,59 +41,83 @@ export const useUsers = () => {
 
   async function loadUsers() {
     setLoading(true);
-    await fetchUsersService()
-      .then(data => {
-        setUsers(data);
-        setLoading(false);
-      })
-      .catch(err => {
-        setError(err.message);
-        setLoading(false);
-      });
+    try {
+      const users = await fetchUsersService()
+      setLoading(false);
+      setUsers(users);
+    }
+    catch (err) {
+      setError(err.message);
+    }
+    finally { setLoading(false); }
   };
 
   async function createUser(userData) {
     setLoading(true);
-    await createUserService(userData)
-      .then(newUser => {
-        setUsers(prevUsers => [newUser, ...prevUsers]);
-      })
-      .catch(err => {
-        setError(err.message);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    try {
+      const newUser = await createUserService(userData);
+      setUsers(prevUsers => [newUser, ...prevUsers]);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  async function userChoosed(user) {
+  async function userChoosed(user, param) {
     setSelectedUser(user);
+    if (param === 'edit') {
+      openEditUserState();
+    } else{
+      openDeletUserState();
+    }
   }
 
   async function updateUser(id, userData) {
     setLoading(true);
-    await updateUserService(id, userData)
-      .then(updateUser => {
-        setUsers(prevUsers => prevUsers.map(user => user.id === id ? updateUser : user))
-        console.log('setUsers');
-      })
-      .catch(err => { setError(err.message) })
-      .finally(() => { setLoading(false) });
+    try {
+      const updateUser = await updateUserService(id, userData);
+      setUsers(prevUsers => prevUsers.map(user => user.id === id ? updateUser : user));
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
   }
 
   async function deleteUser(id) {
     setLoading(true);
-    await deleteUserService(id)
-      .then(() => {
-        setUsers(prevUsers => prevUsers.filter(user=> user.id !== id));
-      })
-      .catch(err => { setError(err.message) })
-      .finally(() => { setLoading(false) });
+    try {
+      await deleteUserService(id);
+      setUsers(prevUsers => prevUsers.filter(user => user.id !== id));
+    } catch (err) {
+      setError(err.message)
+    } finally {
+      setLoading(false)
+    }
   }
 
   useEffect(() => {
     loadUsers();
   }, []);
 
-  return { users, loading, error, createUser, updateUser, userChoosed, selectedUser, deleteUser };
+  return {
+    users,
+    loading,
+    error,
+    createUser,
+    updateUser,
+    userChoosed,
+    selectedUser,
+    deleteUser,
+    openEditUserState,
+    closeEditUserState,
+    openDeletUserState,
+    closeDeletUserState,
+    openCreateNewUserDiv,
+    closeCreateNewUser,
+    addUserState,
+    editUserState,
+    deletUserState
+  };
 };
